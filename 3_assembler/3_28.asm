5a619988d1298aa0c6c5fe3470369e88b1cc7f1c
Anton Shevchuk <ashevchuk22@gmail.com>
2022-04-27 09:54:52 +1100

    Create 3_28.asm

---
+++ 3_assembler/3_28.asm
@@ -0,0 +1,26 @@
+%macro ARRAY_FILL 3
+    %assign i %1
+    %assign j %2
+    %rep %3 
+        dd i 
+        %assign i i+j
+    %endrep
+%endmacro
+
+global _start
+section .text:
+;print elements of array to check
+_start: cmp     ecx, 10
+        je      .quit
+        mov     al, [array+ecx*4]
+        add     al, 0x30
+        PUTCHAR al
+        PUTCHAR 0x20
+        inc     ecx
+        jmp     _start
+.quit:  PUTCHAR 0x0a
+        FINISH
+        
+
+section .data:
+array:  ARRAY_FILL 1, 1, 10
